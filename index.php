7f5134b01aeca791837db32ae8d4670774c88e9b
phefrylyn <lynphefry@gmail.com>
2026-06-05 15:24:06 +0300

    second commit

--- index.php
+++ index.php
@@ -71,5 +71,79 @@ $trainer2->introduce();
 
 ?>
 
+
+
+<h2>Student Registration Form</h2>
+
+<form method="post">
+    <label>Name:</label>
+    <input type="text" name="name" required><br><br>
+
+    <label>Course:</label>
+    <input type="text" name="course" required><br><br>
+
+    <input type="submit" name="submit" value="Register Student">
+</form>
+
+<?php
+
+class Student {
+    public $name;
+    public $course;
+
+    public function introduce() {
+        echo "<h3>Student Details</h3>";
+        echo "Hello, my name is {$this->name}.<br>";
+        echo "I am studying {$this->course}.<br><br>";
+    }
+}
+
+if (isset($_POST['submit'])) {
+    $student = new Student();
+    $student->name = $_POST['name'];
+    $student->course = $_POST['course'];
+
+    $student->introduce();
+}
+
+?>
+
+<hr>
+
+<h2>Trainer Registration Form</h2>
+
+<form method="post">
+    <label>Name:</label>
+    <input type="text" name="trainer_name" required><br><br>
+
+    <label>Specialization:</label>
+    <input type="text" name="specialization" required><br><br>
+
+    <input type="submit" name="trainer_submit" value="Register Trainer">
+</form>
+
+<?php
+
+class Trainer {
+    public $name;
+    public $specialization;
+
+    public function introduce() {
+        echo "<h3>Trainer Details</h3>";
+        echo "Hello, my name is {$this->name}.<br>";
+        echo "I teach {$this->specialization}.<br><br>";
+    }
+}
+
+if (isset($_POST['trainer_submit'])) {
+    $trainer = new Trainer();
+    $trainer->name = $_POST['trainer_name'];
+    $trainer->specialization = $_POST['specialization'];
+
+    $trainer->introduce();
+}
+
+?>
+
 </body>
 </html>
